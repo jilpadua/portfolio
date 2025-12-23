467ae40a3f2b140b6d544961229b1ac26157f003
Jilmer P. <77193911+jilpadua@users.noreply.github.com>
2025-12-23 23:06:23 +0800

    chore: fix footer to always stay at the bottom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,33 +13,33 @@ export default async function HomePage() {
   const page = await client.fetch(HOME_PAGE_QUERY);
 
   return (
-    <>
-      <Navbar />
+  <div className="min-h-screen flex flex-col">
+    <Navbar />
 
-      <main>
-  {page?.sections?.length ? (
-    page.sections.map((section: any, index: number) => {
-      switch (section._type) {
-        case 'hero':
-          return <Hero key={index} data={section} />;
-        case 'experienceSection':
-          return <Experience key={index} data={section} />;
-        case 'projectsSection':
-          return <Projects key={index} data={section} />;
-        case 'contactSection':
-          return <Contact key={index} data={section} />;
-        default:
-          return null;
-      }
-    })
-  ) : (
-    <p className="flex items-center justify-center h-screen text-center text-gray-500 text-lg">
-      🚧 Portfolio in development — content coming soon!
-    </p>
-  )}
-</main>
+    <main className="flex-grow">
+      {page?.sections?.length ? (
+        page.sections.map((section: any, index: number) => {
+          switch (section._type) {
+            case 'hero':
+              return <Hero key={index} data={section} />;
+            case 'experienceSection':
+              return <Experience key={index} data={section} />;
+            case 'projectsSection':
+              return <Projects key={index} data={section} />;
+            case 'contactSection':
+              return <Contact key={index} data={section} />;
+            default:
+              return null;
+          }
+        })
+      ) : (
+        <p className="flex items-center justify-center h-screen text-center text-gray-500 text-lg">
+            🚧 Portfolio in development — content coming soon!
+        </p>
+      )}
+    </main>
 
-      <Footer />
-    </>
-  );
+    <Footer />
+  </div>
+);
 }
