634d2da6720c2874f6364c484f0d7850fa4f3534
Jilmer P. <77193911+jilpadua@users.noreply.github.com>
2026-01-07 10:40:19 +0800

    feat: Add remaining sections and schema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,36 @@
 import { client } from '@/src/sanity/client'
 import { heroQuery } from '@/src/sanity/queries'
+import { aboutQuery } from '@/src/sanity/queries'
+import { skillsQuery } from '@/src/sanity/queries'
+import { projectsQuery } from '@/src/sanity/queries'
+import { experienceQuery } from '@/src/sanity/queries'
+import { footerQuery } from '@/src/sanity/queries'
+
 import Hero from '@/src/components/Sections/Hero'
+import About from '@/src/components/Sections/About'
+import Skills from '@/src/components/Sections/Skill'
+import Projects from '@/src/components/Sections/Projects'
+import Experience from '@/src/components/Sections/Experience'
 import Navbar from '@/src/components/Layout/Navbar'
 import Footer from '@/src/components/Layout/Footer'
 
 export default async function HomePage() {
   const hero = await client.fetch(heroQuery)
+  const about = await client.fetch(aboutQuery)
+  const skills = await client.fetch(skillsQuery)
+  const projects = await client.fetch(projectsQuery)
+  const experience = await client.fetch(experienceQuery)
+  const footer = await client.fetch(footerQuery)
 
   return (
     <>
       <Navbar />
       <Hero data={hero} />
-      <Footer />
+      <About data={about} />
+      <Skills data={skills} />
+      <Projects data={projects} />
+      <Experience data={experience} />
+      <Footer data={footer}/>
     </>
   )
 }
